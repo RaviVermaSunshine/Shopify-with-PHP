4891da84d4ee43a80eee998021b9bdc8fd28ea3b
Ravee Verma Sunshine <ravivermashine@gmail.com>
2018-01-19 16:18:56 +0530

    Update index.php

--- index.php
+++ index.php
@@ -9,15 +9,27 @@
         <br/><br/><br/>
         <h2>Shopify with PHP</h2>
         <main>
-    		<form method="POST" action="install.php">
-    			<fieldset class="flex one">
-    			  <label>
-    			  	Store Name
-    			  	<input type="text" name="store" placeholder="e.g. test-shop.myshopify.com" required>
-    			  </label>
-    			</fieldset>
-    			<button>Install</button>
-    		</form>
+            
+            <?php
+		  	    if(isset($_GET['shop']) && $_GET['shop'] != ''){
+		  	        echo '<h4>Store Name: '.$_GET['shop'].'</h4>';
+		  	    }
+		  	    else{
+  	        ?>
+  	                <form method="POST" action="install.php">
+            			<fieldset class="flex one">
+            			  <label>
+            			  	Store Name
+            			  	<input type="text" name="store" placeholder="e.g. test-shop.myshopify.com" required>
+            			  </label>
+            			</fieldset>
+            			<button>Install</button>
+            		</form>
+  	        <?php
+		  	    }
+		  	?>
+            
+    		
     	</main>
         <br/><br/><br/>
     </body>
